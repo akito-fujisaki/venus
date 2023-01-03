9736505d16d3fd4d623a48863eda87bf265b8f36
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-03 13:33:35 +0900

    20230103/4 NATゲートウェイ、ルートテーブルを作成 (#15)
    - NATゲートウェイ、ルートテーブルを作成
    - コストを抑えるためにNATは1つだけとする

--- terraform/test/vpc.tf
+++ terraform/test/vpc.tf
@@ -52,3 +52,71 @@ resource "aws_subnet" "private_c" {
     Name = "${local.product}-${local.env}-subnet-private-c"
   })
 }
+
+resource "aws_eip" "ngw_a" {
+  vpc = true
+
+  tags = merge(local.default_tags, {
+    Name = "${local.product}-${local.env}-eip-ngw-a"
+  })
+}
+
+# コストを抑えるためにNATは1つだけとする
+resource "aws_nat_gateway" "ngw_a" {
+  allocation_id = aws_eip.ngw_a.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = merge(local.default_tags, {
+    Name = "${local.product}-${local.env}-ngw-a"
+  })
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.default_tags, {
+    Name = "${local.product}-${local.env}-rt-public"
+  })
+}
+
+resource "aws_route" "public_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.default_tags, {
+    Name = "${local.product}-${local.env}-rt-private"
+  })
+}
+
+resource "aws_route" "private_ngw" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw_a.id
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_c" {
+  subnet_id      = aws_subnet.private_c.id
+  route_table_id = aws_route_table.private.id
+}
