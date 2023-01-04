7a984c2fa48aacaa10a310400797fc8da68e2b89
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-05 00:05:49 +0900

    fix

--- infra/cmd/build_task_definition.sh
+++ infra/cmd/build_task_definition.sh
@@ -3,13 +3,13 @@
 set -eu
 
 TASK_DEFINITION=$1
-export IMAGE_URI=$4
-export APP_NAME=$2
-export DEPROY_ENV=$3
+export IMAGE_URI=$2
+export APP_NAME=$3
+export DEPROY_ENV=$4
 
-export TASK_ROLE_ARN=$(aws iam get-role --role-name ${APP_NAME}-${DEPROY_ENV}-ecs-task-role | jq -r '.Role.Arn')
-export TASK_EXECUTION_ROLE_ARN=$(aws iam get-role --role-name ${APP_NAME}-${DEPROY_ENV}-ecs-task-role | jq -r '.Role.Arn')
-export SECRETS_MANAGER_ARN=$(aws secretsmanager list-secrets --filter Key=name,Values=${APP_NAME}-${DEPROY_ENV}| jq -r '.SecretList[0].ARN')
+export TASK_ROLE_ARN=$(aws iam get-role --role-name "${APP_NAME}-${DEPROY_ENV}-ecs-task-role" | jq -r '.Role.Arn')
+export TASK_EXECUTION_ROLE_ARN=$(aws iam get-role --role-name "${APP_NAME}-${DEPROY_ENV}-ecs-task-role" | jq -r '.Role.Arn')
+export SECRETS_MANAGER_ARN=$(aws secretsmanager list-secrets --filter Key=name,Values="${APP_NAME}-${DEPROY_ENV}" | jq -r '.SecretList[0].ARN')
 
 cd $(dirname $0)
 envsubst < ../task_definitions/${TASK_DEFINITION}.json
