2346fab792af1a288541b22cbf61f756bf4eace4
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-04 18:37:30 +0900

    wip

--- infra/cmd/build_task_definition.sh
+++ infra/cmd/build_task_definition.sh
@@ -3,11 +3,11 @@
 APP_NAME=$1
 DEPROY_ENV=$2
 ECR_REGISTRY=$3
-TEMPLATE_TASK_DEFINITION_PATH=$4
+TASK_DEFINITION=$4
 
 export IMAGE_URI=${ECR_REGISTRY}/venus-backend:$(git rev-parse main)
 export TASK_ROLE_ARN=$(aws iam get-role--role-name ${APP_NAME}-${DEPROY_ENV}-ecs-task-role | jq -r '.Role.Arn')
 export TASK_EXECUTION_ROLE_ARN=$(aws iam get-role--role-name ${APP_NAME}-${DEPROY_ENV}-ecs-task-role | jq -r '.Role.Arn')
 export SECRETS_MANAGER_ARN=$(aws secretsmanager list-secrets --filter Key=name,Values=${APP_NAME}-${DEPROY_ENV}| jq -r '.SecretList[0].ARN')
 
-echo $(envsubst < TEMPLATE_TASK_DEFINITION_PATH)
+echo $(envsubst < ../${TASK_DEFINITION})
