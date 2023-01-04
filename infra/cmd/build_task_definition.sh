4fd0f926906443f8dfced3dd8a2d2f4b0f787427
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-05 00:56:19 +0900

    fix

--- infra/cmd/build_task_definition.sh
+++ infra/cmd/build_task_definition.sh
@@ -8,7 +8,7 @@ export APP_NAME=$3
 export DEPROY_ENV=$4
 
 export TASK_ROLE_ARN=$(aws iam get-role --role-name "${APP_NAME}-${DEPROY_ENV}-ecs-task-role" | jq -r '.Role.Arn')
-export TASK_EXECUTION_ROLE_ARN=$(aws iam get-role --role-name "${APP_NAME}-${DEPROY_ENV}-ecs-task-role" | jq -r '.Role.Arn')
+export TASK_EXECUTION_ROLE_ARN=$(aws iam get-role --role-name "${APP_NAME}-${DEPROY_ENV}-ecs-task-execution-role" | jq -r '.Role.Arn')
 export SECRETS_MANAGER_ARN=$(aws secretsmanager list-secrets --filter Key=name,Values="${APP_NAME}-${DEPROY_ENV}" | jq -r '.SecretList[0].ARN')
 
 cd $(dirname $0)
