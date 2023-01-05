7cf743728623f8b2b727eb3925328121ce0c3f90
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-05 19:20:57 +0900

    20230105/7 Sandbox環境用のDBを作成 (#34)
    - Sandbox環境用のDBを作成
    - DBの作成後に、接続情報等をシークレットマネージャーに保存するようにする (リソースの作成/削除を行いやすくするため)

--- infra/terraform/sandbox/random.tf
+++ infra/terraform/sandbox/random.tf
@@ -1,3 +1,13 @@
+# シークレットを削除すると同じ名前でしばらく作成できないため、回避策としてランダムな文字列を末尾に加えるための使用する
 resource "random_id" "main" {
   byte_length = 4
+}
+
+resource "random_id" "rails_secret_key_base" {
+  byte_length = 128
+}
+
+resource "random_password" "database_root_user" {
+  length  = 8
+  special = false
 }
