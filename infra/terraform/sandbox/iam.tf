b58bedbbd6dfb724db4140bf50caeb709ca01259
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-06 09:16:27 +0900

    20230106/1 ECSタスクに対してexecute-commandを使用できるようにする
    - 詳細: https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/userguide/ecs-exec.html

--- infra/terraform/sandbox/iam.tf
+++ infra/terraform/sandbox/iam.tf
@@ -45,6 +45,32 @@ resource "aws_iam_policy" "secrets_manager_read" {
   })
 }
 
+# タスクに対してexecute-commandを使用できるようにする
+# https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/userguide/ecs-exec.html
+resource "aws_iam_policy" "ecs_exec_command" {
+  name        = "${local.app}-${local.env}-ecs-exec-command"
+  description = "${local.app}-${local.env}-ecs-exec-command"
+  path        = "/"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+  tags = merge(local.default_tags, {
+    Name = "${local.app}-${local.env}-ecs-exec-command"
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "amazon_ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = data.aws_iam_policy.amazon_ecs_task_execution_role_policy.arn
@@ -53,6 +79,11 @@ resource "aws_iam_role_policy_attachment" "amazon_ecs_task_execution_role_policy
 resource "aws_iam_role_policy_attachment" "secrets_manager_read" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.secrets_manager_read.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_command" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_exec_command.arn
 }
 
 resource "aws_iam_role" "ecs_task_role" {
