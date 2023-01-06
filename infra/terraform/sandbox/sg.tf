60e95da2ae1856e4afb96a2276ed2f089a4c2c53
Akito Fujisaki <52433677+akito-fujisaki@users.noreply.github.com>
2023-01-06 12:13:08 +0900

    20230106/3 RDSのセキュリティグループの作成と適用
    RDSにアクセスするためには、3306ポートを許可するセキュリティグループを適用する必要があった。
    なお、VPC内のみからのアクセスのみを許可することとする。

--- infra/terraform/sandbox/sg.tf
+++ infra/terraform/sandbox/sg.tf
@@ -84,3 +84,32 @@ resource "aws_security_group_rule" "backend_ingress_3000" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.backend.id
 }
+
+resource "aws_security_group" "rds" {
+  description            = "${local.app}-${local.env}-sg-rds"
+  name                   = "${local.app}-${local.env}-sg-rds"
+  vpc_id                 = aws_vpc.main.id
+  revoke_rules_on_delete = false
+
+  tags = merge(local.default_tags, {
+    Name = "${local.app}-${local.env}-sg-rds"
+  })
+}
+
+resource "aws_security_group_rule" "rds_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds.id
+}
+
+resource "aws_security_group_rule" "rds_ingress" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"]
+  security_group_id = aws_security_group.rds.id
+}
